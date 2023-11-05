0bf770d8eb698aa83a4d516c96271723d0891156
이영진 <144555742+permawintre@users.noreply.github.com>
2023-11-06 08:16:17 +0900

    jsDoc으로 주석달아놓았습니다

--- src/Home/supportFunctions.js
+++ src/Home/supportFunctions.js
@@ -1,11 +1,15 @@
 import moment from 'moment'
 import { useState } from 'react'
-import arrow from '../images/arrow.png'
-import filledStar from '../images/filledStar.png'
-import emptyStar from '../images/emptyStar.png'
-import comments from '../images/comments.png'
+import arrow from './arrow.png'
+import filledStar from './filledStar.png'
+import emptyStar from './emptyStar.png'
+import comments from './comments.png'
 
-//몇시간전에 게시된건지 텍스트로 반환함
+/**
+ * 몇시간 전에 게시된건지 텍스트로 반환하는 함수입니다(moment.js 설치필요)
+ * @param {string} "YYYY-mm-dd HH:MM:SS" 형식으로 날짜 입력
+ * @returns 입력된 날짜와 현재 시각의 차이를 문자열로 리턴
+ */
 export const getDayMinuteCounter = (date) => {
     if (!date) {
       return ''
@@ -13,24 +17,49 @@ export const getDayMinuteCounter = (date) => {
   
     const today = moment()
     const postingDate = moment(date)
+    const yearDiff = postingDate.diff(today, 'years')
+    const monthDiff = postingDate.diff(today, 'months')
     const dayDiff = postingDate.diff(today, 'days')
     const hourDiff = postingDate.diff(today, 'hours')
     const minutesDiff = postingDate.diff(today, 'minutes')
+    const secondsDiff = postingDate.diff(today, 'seconds')
+
+    if (yearDiff === 0 && monthDiff === 0 && dayDiff === 0 && hourDiff === 0 && minutesDiff === 0) { // 작성한지 1분도 안지났을때
+      const seconds = Math.ceil(-secondsDiff)
+      return seconds + '초 전'	 // '초' 로 표시
+    }
     
-    if (dayDiff === 0 && hourDiff === 0) { // 작성한지 1시간도 안지났을때
+    if (yearDiff === 0 && monthDiff === 0 && dayDiff === 0 && hourDiff === 0 && minutesDiff <= 60) { // 작성한지 1시간도 안지났을때
       const minutes = Math.ceil(-minutesDiff)
-      return minutes + '분 전'	 // '분' 로 표시
+      return minutes + '분 전'	 // '분'으로 표시
     }
   
-    if (dayDiff === 0 && hourDiff <= 24) { // 작성한지 1시간은 넘었지만 하루는 안지났을때, 
+    if (yearDiff === 0 && monthDiff === 0 && dayDiff === 0 && hourDiff <= 24) { // 작성한지 1시간은 넘었지만 하루는 안지났을때, 
       const hour = Math.ceil(-hourDiff)
       return hour + '시간 전'		 // '시간'으로 표시
     }
   
-    return -dayDiff + '일 전'		 // '일'로 표시
+    if (yearDiff === 0 && monthDiff === 0 && dayDiff <= 30) { // 작성한지 하루는 넘었지만 한달은 안지났을때, 
+      const day = Math.ceil(-dayDiff)
+      return day + '일 전'		 // '일'로 표시
+    }
+  
+    if (yearDiff === 0 && monthDiff <= 12) { // 작성한지 한달은 넘었지만 1년은 안지났을때, 
+      const month = Math.ceil(-monthDiff)
+      return month + '개월 전'		 // '달'로 표시
+    }
+  
+    return -yearDiff + '년 전'		 // '년'으로 표시
 }
 
-//게시글 짧으면 그냥 표시, 길면 더보기/접기 버튼 표시
+/**
+ * 게시글에 접기, 펼치기 기능을 달아 반환하는 함수형 컴포넌트입니다.
+ * 1) 220자 이하일 때에는 그대로 출력
+ * 2) 220자 이상일 때에는 3줄로 줄이고 더 보기 표시(default)
+ * 2-1) 더 보기 누르면 전체 내용 표시되고 접기 버튼 표시
+ * @param {string} 텍스트 input 
+ * @returns className 'postContents'인 div로 둘러싸인 컴포넌트 반환
+ */
 export const PostContents = ({contents}) => {
 
   const [state,setState] = useState(true);
@@ -43,7 +72,7 @@ export const PostContents = ({contents}) => {
   const contentsLength = JSON.parse(JSON.stringify({contents})).contents.length
   if(contentsLength<=220) {
     return(
-      <div>{contents}</div>
+      <div className='postContents'>{contents}</div>
     )
   }
 
@@ -65,7 +94,11 @@ export const PostContents = ({contents}) => {
   }
 }
 
-//사진 여러장일때 슬라이더로
+/**
+ * 함수형 컴포넌트로, 이미지 배열 넣으면 좌우 슬라이드 가능한 component로 반환합니다(550px * 550px)
+ * @param {Array} 사진으로 구성된 배열 input 
+ * @returns className 'postPic'인 div로 둘러싸인 컴포넌트 반환
+ */
 export const PostPics = ({imgs}) => {
 
   /* 리스너 설치하기 */
@@ -112,6 +145,11 @@ export const PostPics = ({imgs}) => {
   )
 }
 
+/**
+ * state사용해서 버튼 눌린상태 / 눌리지 않은상태 체크
+ * 반응형 애니메이션은 css로 구현
+ * @returns 좋아요 버튼 렌더링
+ */
 export const LikeBtn = () => {
 
   let [state, setState] = useState(false)
@@ -137,6 +175,10 @@ export const LikeBtn = () => {
   
 }
 
+/**
+ * 반응형 애니메이션은 css로 구현
+ * @returns 댓글 버튼 렌더링
+ */
 export const CommentBtn = () => {
 
   return(
